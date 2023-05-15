34a1fbe1040975308a99d449d07f453c1a2f35d2
thomascarre <carrethomas7@gmail.com>
2023-05-15 15:21:44 +0200

    ajout des boutons filtres sur page home

--- sources/pages/index.js
+++ sources/pages/index.js
@@ -22,6 +22,56 @@ async function getWorks() {
     console.error('Une erreur s\'est produite lors de la récupération des works:', error);
   }
 }
+const btnTous = document.querySelector('.btn-all');
+btnTous.addEventListener('click', function () {
+  getWorks();
+});
+
+const btnObjet = document.querySelector('.btn-object');
+btnObjet.addEventListener('click', async () => {
+  try {
+    // Effectue une requête à l'API pour récupérer les travaux
+    const response = await fetch('http://localhost:5678/api/works');
+    const data = await response.json(); // Convertit la réponse en JSON
+
+    // Filtre les travaux pour ne garder que ceux ayant la catégorie "Objets"
+    const filteredWorks = data.filter(work => work.category.name === 'Objets');
+
+    // Affiche les travaux filtrés
+    console.log(filteredWorks);
+  } catch (error) {
+    console.error('Une erreur s\'est produite lors de la récupération des works:', error);
+  }
+});
+
+//Fonction pour ajouter les écouteurs d'événement aux boutons de filtrage
+function setupFilterButtons() {
+  const btnObjet = document.querySelector('.btn-object');
+  const btnAppartement = document.querySelector('.btn-flat');
+  const btnHotel = document.querySelector('.btn-hotel');
+
+
+  // btnTous.addEventListener('click', () => {
+  //   // Affiche tous les travaux
+  //   // getWorks();
+
+  // });
+
+  // btnObjet.addEventListener('click', () => {
+  //   // Filtre les travaux par catégorie "Objet"
+  //   filterWorksByCategory('Objet');
+  // });
+
+  // btnAppartement.addEventListener('click', () => {
+  //   // Filtre les travaux par catégorie "Appartement"
+  //   filterWorksByCategory('Appartement');
+  // });
+
+  // btnHotel.addEventListener('click', () => {
+  //   // Filtre les travaux par catégorie "Hôtel"
+  //   filterWorksByCategory('Hôtel');
+  // });
+}
 
 // Appel de la fonction pour récupérer et afficher les travaux
 getWorks();
